e1ee2654471347aadf20c8a9d7d673a169ef0530
LizandroCanul <canullonalizandro@gmail.com>
2026-02-03 17:26:23 -0600

    Primera carga -modulos

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ObrasModule } from './obras/obras.module';
+
+@Module({
+  imports: [ObrasModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
